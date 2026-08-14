58084486ca6b0b1797e43a10333bd9da46069eef
Leandro Yunoguthi <leandro.yunoguthi@hotmail.com>
2026-08-13 22:05:22 -0300

    botao quantidade

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './core/header/header';
 import { Footer } from "./core/footer/footer";
+import { QuantidadeControle } from './shared/quantidade-controle/quantidade-controle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, QuantidadeControle],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
