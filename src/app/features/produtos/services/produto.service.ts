047f0b41ec7837570f9be08634022f32a0f58453
matledarioo <matheusvlgk2@gmail.com>
2026-09-03 21:56:21 -0300

    aula5

---
+++ src/app/features/produtos/services/produto.service.ts
@@ -0,0 +1,49 @@
+import { inject, Injectable } from '@angular/core';
+import { LoggerService } from '../../../core/services/logger/logger.service';
+import { Produto } from '../../../model/produto';
+import { delay, Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProdutoService {
+
+  private logger = inject(LoggerService);
+
+  private readonly listaMock = <Produto[]>[
+    {
+      id: 1,
+      nome: 'Mounjaro',
+      preco: 1699,
+      descricao: 'Canetas 1',
+      imageUrl: 'images/mounjaro.jpg',
+      promo: false,
+      estado: 'novo'
+    },
+    {
+      id: 2,
+      nome: 'Ozempic',
+      preco: 1299,
+      descricao: 'Canetas 2',
+      imageUrl: 'images/ozempic.jpg',
+      promo: false,
+      estado: 'usado'
+    },
+    {
+      id: 3,
+      nome: 'Wegovy',
+      preco: 2500,
+      descricao: 'Canetas 3',
+      imageUrl: 'images/wegovy.jpg',
+      promo: true,
+      estado: 'novo'
+    },
+  ];
+
+  listar(): Observable<Produto[]>{
+    this.logger.info("[PRODUTO SERVICE] - Retornando lista de produtos")
+    return of(this.listaMock).pipe(
+      delay(250)
+    );
+  }
+}
